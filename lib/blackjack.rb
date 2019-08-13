8fc429fd9200e67a9f286f8d7c1da3385e0b1645
Eugenia Kim <elk303@nyu.edu>
2019-08-13 21:37:06 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -41,6 +41,8 @@ def hit?(card_total)
   if get_user_input == 's'
     if get_user_input == 'h'
       return deal_card
+      deal_card + card_total = card_total
+      return card_total
    
 end
 
